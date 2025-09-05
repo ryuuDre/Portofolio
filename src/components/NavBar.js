54c7a7a3b8b95a02d7b2c2b1effcf8a2dc89402d
Ryuu <63155101+ryuuDre@users.noreply.github.com>
2025-09-05 11:28:07 +0700

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -89,16 +89,13 @@ const NavBar = () => {
           </nav>
 
           <nav className="flex items-center justify-center flex-wrap mt-2">
-            <motion.a href="https://github.com/AndreMaulaRaufiq" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mr-3 sm:mx-1 bg-light rounded-full dark:bg-dark">
+            <motion.a href="https://github.com/ryuuDre" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mr-3 sm:mx-1 bg-light rounded-full dark:bg-dark">
               <GithubIcon />
             </motion.a>
             <motion.a href="https://www.linkedin.com/andre-maula-r" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mx-3 sm:mx-1">
               <LinkedInIcon />
             </motion.a>
-            <motion.a href="https://x.com/Ryuu_145" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mx-3 sm:mx-1">
-              <TwitterIcon />
-            </motion.a>
-            <motion.a href="https://www.instagram.com/andremaularaufiq/" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 ml-3 sm:mx-1">
+            <motion.a href="https://www.instagram.com/ryuu.dre/" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 ml-3 sm:mx-1">
               <InstagramIcon />
             </motion.a>
 
