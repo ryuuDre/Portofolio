dfbacb4dab4b724d11dfe291f783c587fd3aa888
Ryuu <63155101+ryuuDre@users.noreply.github.com>
2025-09-05 11:33:45 +0700

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -56,16 +56,13 @@ const NavBar = () => {
         </nav>
 
         <nav className="flex items-center justify-center flex-wrap">
-          <motion.a href="https://github.com/AndreMaulaRaufiq" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mr-3">
+          <motion.a href="https://github.com/ryuuDre" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mr-3">
             <GithubIcon />
           </motion.a>
           <motion.a href="https://www.linkedin.com/andre-maula-r" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mx-3">
             <LinkedInIcon />
           </motion.a>
-          <motion.a href="https://x.com/Ryuu_145" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mx-3">
-            <TwitterIcon />
-          </motion.a>
-          <motion.a href="https://www.instagram.com/andremaularaufiq/" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 ml-3">
+          <motion.a href="https://www.instagram.com/ryuu.dre/" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 ml-3">
             <InstagramIcon />
           </motion.a>
 
